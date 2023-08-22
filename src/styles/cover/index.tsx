71e5b1a685fbe36248ec38883acdc65c6a210712
arpitkarnatak <arpitkarnatak47@gmail.com>
2023-08-22 12:39:56 +0530

    Added minor fe fix

--- src/styles/cover/index.tsx
+++ src/styles/cover/index.tsx
@@ -5,6 +5,7 @@ export default function LoadingPlaceholderAndCover() {
   return (
     <div
       style={{
+        top: "0",
         width: "100%",
         height: "100%",
         background: "#008080",
