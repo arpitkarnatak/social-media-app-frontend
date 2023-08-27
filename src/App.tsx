6d66ca407619522ed05a50243d7d444560df0ab8
arpitkarnatak <arpitkarnatak47@gmail.com>
2023-08-28 01:02:43 +0530

    Added lazy loading, dockerizing fe

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import GlobalContextProvider from "context/GlobalContext";
-import Home from "pages/Home";
 import Header from "components/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Profile from "pages/Profile";
-import Post from "pages/Post";
 import ScrollToTopButton from "components/ScrollToTop";
-import { Title36, Title64 } from "styles/typography";
+import LoadingPlaceholderAndCover from "styles/cover";
 
 function App() {
   const queryClient = new QueryClient({
@@ -19,6 +16,10 @@ function App() {
     },
   });
 
+  const Home = lazy(() => import("pages/Home"))
+  const Profile = lazy(() => import("pages/Profile"))
+  const Post = lazy(() => import("pages/Post"))
+  
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalContextProvider>
@@ -26,11 +27,13 @@ function App() {
           <Header />
           <ScrollToTopButton />
           <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/user/:userId" element={<Profile />} />
-              <Route path="/post/:postId" element={<Post />} />
-            </Routes>
+            <Suspense fallback={<LoadingPlaceholderAndCover />}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/user/:userId" element={<Profile />} />
+                <Route path="/post/:postId" element={<Post />} />
+              </Routes>
+            </Suspense>
           </BrowserRouter>
         </div>
       </GlobalContextProvider>
